ef86ad9b92819ca1b3b684e95647100f61c88021
Matthew Casperson <matthewcasperson@gmail.com>
2021-07-01 08:05:41 +1000

    logged port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const port = parseInt(process.env.PORT, 10) || 5000;
 const name = process.env.MYNAME || "LogRocket"
 
+console.log(port)
+
 http.createServer((request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/plain'
